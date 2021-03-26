9099c2c71800fc4089bd93264aeec13c3115267a
webmaster <webmaster@div-it.com.ar>
2021-03-25 22:22:21 -0300

    Agregando reset

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -3,7 +3,7 @@ import {Request, Response} from  'express';
 import {User} from  '../entity/User';
 import * as jwt from 'jsonwebtoken';
 import config from '../config/config';
-
+import {validate} from 'class-validator';
 
 class AuthController{
     static login =  async (req:Request,  res:Response) => {
@@ -23,6 +23,33 @@ class AuthController{
             return res.json({message:'Ok',token});       
         }
         return res.status(400).json({message:'The user or password are incorrect!'});        
-    }    
+    }   
+
+    static changePassword = async(req:Request, res:Response) =>{
+        const {userId} = res.locals.jwtPayload;
+        const {oldPassword, newPassword} = req.body;
+        if(!(oldPassword && newPassword)){
+            return res.status(400).json({message:'OldPasword and newPassword are required!'});
+        }        
+        let user:User;
+        const userRepository = getRepository(User);
+        try {
+            user = await userRepository.findOneOrFail(userId);            
+        } catch (error) {
+            return res.status(404).json({message:'User not found!'});
+        }
+        if(!user.checkPassword(oldPassword)){
+            return res.status(401).json({message:'OldPassword did not match!'});
+        }
+        user.password = newPassword;        
+        const validateOptions = {validationError:{target:false, value:false}};
+        const errors = await validate(user, validateOptions);        
+        if( errors.length){
+            return res.status(401).json({errors});
+        }
+        user.hashPassword();        
+        userRepository.save(user);        
+        res.json({message:'Password changed!'});        
+    }
 }
 export default AuthController;
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,10 @@
 import {Router} from 'express';
 import AuthController from '../controller/AuthController';
+import { checkJwt } from '../midlewares/jtw';
 
 const router = Router();
 router.post('/login',AuthController.login);
+
+router.post('/reset',[checkJwt],AuthController.changePassword);
 
 export default router;
