0cce395366b6eeb63d077d1bbb9623a677c5d0ab
Lucas Avila <lucas_avila@div-it.com.ar>
2021-04-01 21:31:58 -0300

    ignore y cors

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,13 @@ createConnection(
 
     // create express app
     const app = express();
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", '*');
+      res.header("Access-Control-Allow-Credentials", true);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+      next();
+    });
     app.use(cors());
     app.use(helmet()); 
     app.use(express.json());  
