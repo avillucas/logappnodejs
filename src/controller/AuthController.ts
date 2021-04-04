3658fb65ff549b5a2c738be375b27e4863b6fefd
Lucas Avila <lucas_avila@div-it.com.ar>
2021-04-03 23:43:11 -0300

    corrigiendo validacion de registro

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,71 +1,76 @@
-import {getRepository} from  'typeorm';
-import {Request, Response} from  'express';
-import {User} from  '../entity/User';
+import { getRepository } from 'typeorm';
+import { Request, Response } from 'express';
+import { User } from '../entity/User';
 import * as jwt from 'jsonwebtoken';
 import config from '../config/config';
-import {validate} from 'class-validator';
+import { validate } from 'class-validator';
 
-class AuthController{
-    static login =  async (req:Request,  res:Response) => {
-        const {username,password} = req.body;
-        if(!(username && password)) {
-            return res.status(400).json({message:'Username & Password are required!'});
+class AuthController {
+    static login = async (req: Request, res: Response) => {
+        const { username, password } = req.body;
+        if (!(username && password)) {
+            return res.status(400).json({ message: 'Username & Password are required!' });
         }
         const userRepository = getRepository(User);
-        let user:User;
-        try{
-            user = await userRepository.findOneOrFail({where:{username}});
-        }catch(e){
-            return res.status(400).json({message:'The user or password are incorrect!'});
+        let user: User;
+        try {
+            user = await userRepository.findOneOrFail({ where: { username } });
+        } catch (e) {
+            return res.status(400).json({ message: 'The user or password are incorrect!' });
         }
-        if(user.checkPassword(password)){
-            const token =  jwt.sign({userId:user.id, username:user.username},config.jwtSecret,{expiresIn:'1h'});
-            return res.json({message:'Ok',token});       
+        if (user.checkPassword(password)) {
+            const token = jwt.sign({ userId: user.id, username: user.username }, config.jwtSecret, { expiresIn: '1h' });
+            return res.json({ message: 'Ok', token });
         }
-        return res.status(400).json({message:'The user or password are incorrect!'});        
-    }   
-
-    static register = async(req:Request, res:Response) =>{
-        const {username, password} = req.body;          
-        const user = new User();
-        user.username = username;
-        user.role = config.Roles.editor;
-        user.setPassword(password);        
-        const validateOptions = {validationError:{target:false, value:false}};
-        const errors = await validate(user, validateOptions);        
-        if( errors.length){
-            return res.status(401).json({errors});
-        }        
-        const userRepository = getRepository(User);        
-        userRepository.save(user);        
-        res.json({message:'User created!'});        
+        return res.status(400).json({ message: 'The user or password are incorrect!' });
     }
 
-    static changePassword = async(req:Request, res:Response) =>{
-        const {userId} = res.locals.jwtPayload;
-        const {oldPassword, newPassword} = req.body;
-        if(!(oldPassword && newPassword)){
-            return res.status(400).json({message:'OldPasword and newPassword are required!'});
-        }        
-        let user:User;
+    static register = async (req: Request, res: Response) => {
+        const { username, password } = req.body;
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        user.role = config.Roles.editor;
+        const validateOptions = { validationError: { target: false, value: false } };
+        const errors = await validate(user, validateOptions);
+        if (errors.length) {
+            return res.status(401).json({ errors });
+        }
+        user.setPassword(password);        
+        try{
+            const userRepository = getRepository(User);        
+            await userRepository.save(user);
+        }catch(e){
+            return res.status(409).json({message:'User already exists!'});
+        }
+        res.json({ message: 'User created!' });
+    }
+
+    static changePassword = async (req: Request, res: Response) => {
+        const { userId } = res.locals.jwtPayload;
+        const { oldPassword, newPassword } = req.body;
+        if (!(oldPassword && newPassword)) {
+            return res.status(400).json({ message: 'OldPasword and newPassword are required!' });
+        }
+        let user: User;
         const userRepository = getRepository(User);
         try {
-            user = await userRepository.findOneOrFail(userId);            
+            user = await userRepository.findOneOrFail(userId);
         } catch (error) {
-            return res.status(404).json({message:'User not found!'});
+            return res.status(404).json({ message: 'User not found!' });
         }
-        if(!user.checkPassword(oldPassword)){
-            return res.status(401).json({message:'OldPassword did not match!'});
+        if (!user.checkPassword(oldPassword)) {
+            return res.status(401).json({ message: 'OldPassword did not match!' });
         }
-        user.password = newPassword;        
-        const validateOptions = {validationError:{target:false, value:false}};
-        const errors = await validate(user, validateOptions);        
-        if( errors.length){
-            return res.status(401).json({errors});
+        user.password = newPassword;
+        const validateOptions = { validationError: { target: false, value: false } };
+        const errors = await validate(user, validateOptions);
+        if (errors.length) {
+            return res.status(401).json({ errors });
         }
         user.setPassword(newPassword);
-        userRepository.save(user);        
-        res.json({message:'Password changed!'});        
+        userRepository.save(user);
+        res.json({ message: 'Password changed!' });
     }
 }
 export default AuthController;
