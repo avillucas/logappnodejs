4fc1b7e751caef0db27ccab944253055835a7158
Lucas Avila <lucas_avila@div-it.com.ar>
2021-05-02 16:47:34 -0300

    a

--- src/controller/VoteController.ts
+++ src/controller/VoteController.ts
@@ -9,36 +9,31 @@ export class VoteController {
    
    //POST 
    static new = async(req:Request, res:Response) => {
-        const validationOptions = {validationError:{target:false, value:false}};    
-        const userId = req.userId;
-        const {votePositive,photoId} = req.body;        
+    let vote = new Vote();
+    const validationOptions = {validationError:{target:false, value:false}};    
+    const userId = req.userId;
+    const {votePositive,photoId} = req.body;        
+    try{          
         const user = await getRepository(User).findOneOrFail(userId);
-        const photo = await getRepository(Photo).findOneOrFail(photoId);
-        //
-        let vote = new Vote();
-        vote.photo = photo;
-        vote.voter = user;
-        vote.like = votePositive as boolean;
-        const voteErrors =await validate(vote,validationOptions);    
-        if(voteErrors.length){
-            return res.status(400).json(voteErrors);        
-        }
-        //
-        try{          
-            await getRepository(Vote).save(vote);        
-        }catch(e){
-            return res.status(409).json({message:'Vote could not be created!'});
-        }    
+        let photo = await getRepository(Photo).findOneOrFail(photoId);
         if(votePositive){
             photo.likes += 1;
         }else{
             photo.dislikes += 1;
         }
-        try{          
-            await getRepository(Photo).save(photo);        
-        }catch(e){
-            return res.status(409).json({message:'Vote could not be created!'});
-        }    
+        await getRepository(Photo).save(photo);        
+        //
+        vote.photo = photoId;
+        vote.voter = user;
+        vote.like = votePositive as boolean;
+        const voteErrors =await validate(vote,validationOptions);    
+        if(voteErrors.length){
+            return res.status(400).json(voteErrors);        
+        }                
+        await getRepository(Vote).save(vote);                        
+    }catch(e){
+        return res.status(409).json({message:'Vote could not be created!'});
+    }    
         res.status(201).json({message:'Vote created!',vote});
     }   
     
